6b518de3e789f770348890873baa6bd6fe4fa519
Naoyuki Eida <v-naeida@microsoft.com>
2020-06-10 17:07:37 +0900

    17時変更です。

--- ODBCConnectTEST/ODBCConnectTEST/Form1.cs
+++ ODBCConnectTEST/ODBCConnectTEST/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.SqlClient; 
 
 namespace ODBCConnectTEST
 {
@@ -15,6 +17,99 @@ namespace ODBCConnectTEST
         public Form1()
         {
             InitializeComponent();
+
+           
+        
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //string constr = @"Server = 172.17.156.238\SQLEXPRESS; database = TEST; UID=sa1;PWD=password01!!";
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"172.17.156.238\SQLEXPRESS";
+            builder.InitialCatalog = "TEST";
+            builder.UserID = "sa1";
+            builder.Password = "password01!!";
+
+            //using (OdbcConnection connection = new OdbcConnection(constr))
+
+            this.textBox1.Text = builder.ConnectionString;  
+
+            using (SqlConnection connection=new SqlConnection())
+            {
+
+                connection.ConnectionString = builder.ConnectionString;  
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT * FROM Tbl1";
+                cmd.Connection = connection;
+
+                
+                SqlDataReader r;
+                r = cmd.ExecuteReader();
+
+                string str1 = string.Empty; 
+                
+                while (r.Read())
+                {
+                    str1 += r[1] + " " + r[2] + "\r\n";
+                }
+
+
+                MessageBox.Show(str1);  
+                
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OdbcConnectionStringBuilder builder = new OdbcConnectionStringBuilder();
+
+
+            //builder.Driver = "{SQL Server}";
+            //builder["Server"] = @"172.17.156.238\SQLEXPRESS;NewValue=SQLServer172.17.156.238";
+            //builder.Dsn = @"SQLServer172.17.156.238";
+            //builder.Add["UID"]="sa1";
+
+            builder.ConnectionString = @"Driver={SQL Server};Server=172.17.156.238\SQLEXPRESS;Database=TEST;Uid=sa1;Pwd=password01!!";
+
+            //MessageBox.Show(builder.ConnectionString);
+            this.textBox2.Text = builder.ConnectionString;   
+
+            using (OdbcConnection connection = new OdbcConnection(builder.ConnectionString))
+            {
+
+                OdbcCommand cmd = new OdbcCommand();
+                cmd.CommandText = "SELECT * FROM Tbl1";
+
+                connection.Open();  
+                
+                cmd.Connection = connection;
+
+                OdbcDataReader r;
+                r = cmd.ExecuteReader();
+
+                string str1 = string.Empty;
+
+                while (r.Read())
+                {
+                    str1 += r[1] + " " + r[2] + "\r\n";
+                }
+
+
+                MessageBox.Show(str1);
+
+            }
+
+
         }
     }
 }
