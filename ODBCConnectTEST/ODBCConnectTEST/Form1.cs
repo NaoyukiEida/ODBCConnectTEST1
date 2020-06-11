d2d63ac40f7395b49fffbd84848aa38d233ec730
Naoyuki Eida <v-naeida@microsoft.com>
2020-06-11 09:48:33 +0900

    06110948

--- ODBCConnectTEST/ODBCConnectTEST/Form1.cs
+++ ODBCConnectTEST/ODBCConnectTEST/Form1.cs
@@ -12,8 +12,11 @@ using System.Data.SqlClient;
 
 namespace ODBCConnectTEST
 {
+   
     public partial class Form1 : Form
     {
+        String SERVER_IPADDRESS= "172.17.247.68";
+
         public Form1()
         {
             InitializeComponent();
@@ -30,7 +33,7 @@ namespace ODBCConnectTEST
             //string constr = @"Server = 172.17.156.238\SQLEXPRESS; database = TEST; UID=sa1;PWD=password01!!";
 
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = @"172.17.156.238\SQLEXPRESS";
+            builder.DataSource = SERVER_IPADDRESS + @"\SQLEXPRESS";
             builder.InitialCatalog = "TEST";
             builder.UserID = "sa1";
             builder.Password = "password01!!";
@@ -79,7 +82,7 @@ namespace ODBCConnectTEST
             //builder.Dsn = @"SQLServer172.17.156.238";
             //builder.Add["UID"]="sa1";
 
-            builder.ConnectionString = @"Driver={SQL Server};Server=172.17.156.238\SQLEXPRESS;Database=TEST;Uid=sa1;Pwd=password01!!";
+            builder.ConnectionString = @"Driver={SQL Server};Server=" + SERVER_IPADDRESS + @"\SQLEXPRESS;Database=TEST;Uid=sa1;Pwd=password01!!";
 
             //MessageBox.Show(builder.ConnectionString);
             this.textBox2.Text = builder.ConnectionString;   
